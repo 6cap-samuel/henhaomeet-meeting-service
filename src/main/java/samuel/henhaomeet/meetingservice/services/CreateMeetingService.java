eceabee84f04f22ac01cf0cde20694564b77590f
samueltoh <samueltoh93@gmail.com>
2022-04-03 14:17:33 +0800

    [SETUP] adding in maven step in github workflows

--- src/main/java/samuel/henhaomeet/meetingservice/services/CreateMeetingService.java
+++ src/main/java/samuel/henhaomeet/meetingservice/services/CreateMeetingService.java
@@ -2,11 +2,9 @@ package samuel.henhaomeet.meetingservice.services;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import samuel.henhaomeet.meetingservice.models.Meeting;
 import samuel.henhaomeet.meetingservice.repositories.CreateMeetingAdapter;
-import samuel.henhaomeet.meetingservice.repositories.MeetingRepository;
 
 @Service
 @AllArgsConstructor
